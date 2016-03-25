331ed35efcd79766310c1a98703c17cc521f6f0c
Aaron Schlesinger <aschlesinger@deis.com>
2016-03-25 12:46:56 -0700

    creating pods for a configurable amount of time

--- config.go
+++ config.go
@@ -8,6 +8,7 @@ import (
 // See github.com/kelseyhightower/envconfig for more information
 type Config struct {
 	NumGoroutines int    `envconfig:"NUM_GOROUTINES" default:"10000"`
+	TimeSec       int    `envconfig:"TIME_SEC" default:"86400"` // 1 day
 	Namespace     string `envconfig:"NAMESPACE" default:"k8stress"`
 }
 
--- main.go
+++ main.go
@@ -3,13 +3,53 @@ package main
 import (
 	"fmt"
 	"log"
+	"math/rand"
 	"os"
 	"sync"
+	"time"
 
 	"github.com/kelseyhightower/envconfig"
+	"github.com/pborman/uuid"
 	"k8s.io/kubernetes/pkg/api"
 	kcl "k8s.io/kubernetes/pkg/client/unversioned"
 )
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func work(i int, pods kcl.PodInterface, podName, namespace string, wg *sync.WaitGroup, t *time.Timer) {
+	defer wg.Done()
+	for {
+		select {
+		case <-t.C:
+			return
+		default:
+		}
+		pod := &api.Pod{
+			ObjectMeta: api.ObjectMeta{
+				Name:      fmt.Sprintf("%s-%s", podName, uuid.New()),
+				Namespace: namespace,
+			},
+			Spec: api.PodSpec{
+				Containers: []api.Container{
+					api.Container{
+						Name:            "alpine-echo",
+						Image:           "alpine:3.3",
+						Command:         []string{"echo", fmt.Sprintf(`"hello k8stress pod %d"`, i)},
+						ImagePullPolicy: api.PullIfNotPresent,
+					},
+				},
+			},
+		}
+		newPod, err := pods.Create(pod)
+		if err != nil {
+			log.Printf("Error creating pod %d (%s)", i, err)
+			return
+		}
+		log.Printf("New pod #%d created: %+v", i, *newPod)
+	}
+}
 
 func main() {
 	conf := new(Config)
@@ -27,34 +67,14 @@ func main() {
 	log.Printf("Running on host %s with config %s", host, conf)
 	pods := client.Pods(conf.Namespace)
 	var wg sync.WaitGroup
+	timer := time.NewTimer(time.Duration(conf.TimeSec) * time.Second)
+	defer timer.Stop()
 	for i := 0; i < conf.NumGoroutines; i++ {
 		wg.Add(1)
-		go func(i int) {
-			defer wg.Done()
-			pod := &api.Pod{
-				ObjectMeta: api.ObjectMeta{
-					Name:      fmt.Sprintf("k8stress-pod-%s-%d", host, i),
-					Namespace: conf.Namespace,
-				},
-				Spec: api.PodSpec{
-					Containers: []api.Container{
-						api.Container{
-							Name:            "alpine-echo",
-							Image:           "alpine:3.3",
-							Command:         []string{"echo", fmt.Sprintf(`"hello k8stress pod %d"`, i)},
-							ImagePullPolicy: api.PullIfNotPresent,
-						},
-					},
-				},
-			}
-			newPod, err := pods.Create(pod)
-			if err != nil {
-				log.Printf("Error creating pod %d (%s)", i, err)
-				return
-			}
-			log.Printf("New pod #%d created: %+v", i, *newPod)
-		}(i)
+		podName := fmt.Sprintf("k8stress-pod-%s-%d", host, i)
+		go work(i, pods, podName, conf.Namespace, &wg, timer)
 	}
+	log.Printf("Done creating %d goroutines", conf.NumGoroutines)
 	wg.Wait()
-	log.Printf("Done creating %d pods", conf.NumGoroutines)
+	log.Printf("Done")
 }
