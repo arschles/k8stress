3201cd85a3349dc7e571d6cb3cfc696094a23241
Aaron Schlesinger <aschlesinger@deis.com>
2016-03-25 09:24:14 -0700

    compiling w/o cgo for alpine

--- make/docker-dev.mk
+++ make/docker-dev.mk
@@ -3,5 +3,5 @@
 CONTAINER_REPO_PATH := github.com/arschles/k8stress
 DEV_ENV_IMAGE := quay.io/deis/go-dev:0.9.1
 DEV_ENV_WORK_DIR := /go/src/github.com/arschles/k8stress
-DEV_ENV_PREFIX := docker run --rm -e GO15VENDOREXPERIMENT=1 -v ${CURDIR}:${DEV_ENV_WORK_DIR} -w ${DEV_ENV_WORK_DIR}
+DEV_ENV_PREFIX := docker run --rm -e GO15VENDOREXPERIMENT=1 -e CGO_ENABLED=0 -v ${CURDIR}:${DEV_ENV_WORK_DIR} -w ${DEV_ENV_WORK_DIR}
 DEV_ENV_CMD := ${DEV_ENV_PREFIX} ${DEV_ENV_IMAGE}
